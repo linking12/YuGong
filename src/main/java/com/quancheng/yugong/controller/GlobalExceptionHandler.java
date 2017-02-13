fe1cee6bebc5245d455fcf1bb7518fa372347c5b
刘石明 <liushiming@quancheng-ec.com>
2017-02-13 19:18:29 +0800

    up

--- src/main/java/com/quancheng/yugong/controller/GlobalExceptionHandler.java
+++ src/main/java/com/quancheng/yugong/controller/GlobalExceptionHandler.java
@@ -5,7 +5,7 @@
  * use it only in accordance with the terms of the license agreement you entered
  * into with Quancheng-ec.com.
  */
-package com.quancheng.yugong.error;
+package com.quancheng.yugong.controller;
 
 import javax.servlet.http.HttpServletRequest;
 

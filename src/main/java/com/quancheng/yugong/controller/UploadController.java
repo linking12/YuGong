e101058ab0992c021ac7354eb4b34fab26c355f4
刘石明 <liushiming@quancheng-ec.com>
2017-02-16 10:59:04 +0800

    up

--- src/main/java/com/quancheng/yugong/controller/UploadController.java
+++ src/main/java/com/quancheng/yugong/controller/UploadController.java
@@ -7,9 +7,15 @@
  */
 package com.quancheng.yugong.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author shimingliu 2017年2月15日 下午6:19:30
@@ -18,9 +24,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UploadController {
 
-    @RequestMapping(value = "/upload", method = RequestMethod.GET)
-    public String index() {
+    private static final String CSV_UPLOAD_DIR = System.getProperty("user.dir") + "/csv/";
+
+    @RequestMapping(value = "/uploadList", method = RequestMethod.GET)
+    public String index(Model model) {
+        File file = new File(CSV_UPLOAD_DIR);
+        model.addAttribute("files", file.listFiles());
         return "upload/uploadFile";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String uploadingPost(@RequestParam("uploadingFiles") MultipartFile[] uploadingFiles) throws IOException {
+        for (MultipartFile uploadedFile : uploadingFiles) {
+            File file = new File(CSV_UPLOAD_DIR + uploadedFile.getOriginalFilename());
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            uploadedFile.transferTo(file);
+        }
+
+        return "redirect:/uploadList";
     }
 
 }

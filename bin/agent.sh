293093058c76b11f8875c25b9403154f1693490f
刘石明 <liushiming@quancheng-ec.com>
2017-03-09 11:24:50 +0800

    up

--- bin/agent.sh
+++ bin/agent.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 SOURCE="$0"
 while [ -h "$SOURCE"  ]; do # resolve $SOURCE until the file is no longer a symlink
     DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
